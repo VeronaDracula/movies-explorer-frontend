8a0f9299bbfc5eacd25e3c3b031be31dadd7164a
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2022-01-28 15:27:13 +0300

    duration

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -16,16 +16,16 @@ function Movies(props) {
     const [keyword, setKeyword] = React.useState(undefined);
     const [cards, setCards] = React.useState([]);
     const [isPreloaderActive, setIsPreloaderActive] = React.useState(false);
-    const [filterDuration, setFilterDuration] = React.useState(false);
+    const [filterDuration, setFilterDuration] = React.useState(JSON.parse(localStorage.filterDuration));
     const [errorText, setErrorText] = React.useState(false);
     const [clickSearch, setClickSearch] = React.useState(false);
     const [clickDuration, setClickDuration] = React.useState(false);
     const [notFound, setNotFound] = React.useState(false);
 
-
-    React.useEffect(() => {
-        setFilterDuration(JSON.parse(localStorage.filterDuration));
-    }, [clickDuration]);
+    //
+    // React.useEffect(() => {
+    //     setFilterDuration(JSON.parse(localStorage.filterDuration));
+    // }, [clickDuration]);
 
     //отслеживаем нажатие кнопки
     function listenerClick(button, setButton) {
